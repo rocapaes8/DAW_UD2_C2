928c1fb2129c9d5a4362042f9f35b5c48fbb6769
Your Name <you@example.com>
2019-10-23 17:55:50 +0200

    inicio

---
+++ adios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Php UD2 Caso practico</title>
+  </head>
+  <body>
+    <?php
+      echo "Hasta luego desarrollador!!! ";
+    ?>
+  </body>
+ </html>
---
+++ hola.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php
+      echo "Hola Paco campeon!!!!";
+    ?>
+  </body>
+ </html>
